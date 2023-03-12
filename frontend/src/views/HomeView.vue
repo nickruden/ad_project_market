720e013275d5d881fb2ccfb4994f9f61a72ea85e
nickruden <1nikitos.ru2018@gmail.com>
2023-03-13 01:19:16 +0300

    Создание компонента HomeView.vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row justify="center">
+      <h1>Home</h1>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  date() {
+    return {
+
+    }
   }
 }
 </script>
