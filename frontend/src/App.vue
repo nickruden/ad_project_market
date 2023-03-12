6c2adcf4a13ac61523c8f1090a620bfba69134ec
nickruden <1nikitos.ru2018@gmail.com>
2023-03-13 00:53:40 +0300

    Добавление иконки к кнопке

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <v-app>
-  <v-navigation-drawer app></v-navigation-drawer> 
-  <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text>Link One</v-btn> 
-       <v-btn  text>Link Two</v-btn> 
-       <v-btn  text>Link Three</v-btn>
-    </v-toolbar-items>
-  </v-app-bar>
-  <v-main>
+    <v-navigation-drawer app></v-navigation-drawer> 
+    <v-app-bar app dark color="primary">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn  text> <v-icon start icon="mdi-cake-variant"></v-icon> Link One</v-btn> 
+        <v-btn  text>Link Two</v-btn> 
+        <v-btn  text>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-main>
     
-  </v-main>
+    </v-main>
   </v-app>
 </template>
 
