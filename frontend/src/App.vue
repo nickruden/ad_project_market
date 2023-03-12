a6822745a73c6321ea34d789e2fb06e0917dadd5
nickruden <1nikitos.ru2018@gmail.com>
2023-03-13 01:13:43 +0300

    Создание динамического меню

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,11 +15,11 @@
       <v-divider></v-divider>
 
       <v-list dense>
-        <v-list-item>
+        <v-list-item v-for="link in links" :key="link.title">
           <template v-slot:prepend>
-            <v-icon icon="mdi-cake-variant"></v-icon>
+            <v-icon :icon="link.icon"></v-icon>
           </template>
-          <v-list-item-title>Link One</v-list-item-title>
+          <v-list-item-title>{{ link.title }}</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer> 
@@ -27,9 +27,10 @@
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn  text> <v-icon start icon="mdi-cake-variant"></v-icon> Link One</v-btn> 
-        <v-btn  text>Link Two</v-btn> 
-        <v-btn  text>Link Three</v-btn>
+        <v-btn text v-for="link in links" :key="link.title">
+          <v-icon start :icon="link.icon"></v-icon> 
+          {{ link.title }} 
+        </v-btn> 
       </v-toolbar-items>
     </v-app-bar>
     <v-main>
@@ -43,6 +44,13 @@
     data() {
       return {
         drawer: false,
+        links: [
+          {title:"Login", icon:"mdi-lock", url:"/login"},
+          {title:"Registration", icon:"mdi-face", url:"/registration"},
+          {title:"Orders",icon:"mdi-bookmark-multiple-outline", url:"/orders"},
+          {title:"New ad", icon:"mdi-note-plus-outline", url:"/new"},
+          {title:"My ads", icon:"mdi-view-list-outline", url:"/list"}
+        ]
       }
     }
   }
