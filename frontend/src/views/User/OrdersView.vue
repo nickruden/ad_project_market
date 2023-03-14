db95cd1817e88a6a106e0fc1492db5aacc7ee055
nickruden <1nikitos.ru2018@gmail.com>
2023-03-14 15:35:26 +0300

    Подготовка и подключение данных для экрана заказов

--- frontend/src/views/User/OrdersView.vue
+++ frontend/src/views/User/OrdersView.vue
@@ -2,19 +2,22 @@
   <v-container>
     <v-row>
       <v-col cols="6" offset="3">
-        <h1 class="text--secondary mb-3 mt-3"> Orders </h1>
+        <h1 class="text--secondary mb-3 mt-3">Orders</h1>
         <v-list subheader two-line flat>
-          <v-list-item value="notifications">
-            <template v-slot:prepend="{ isActive }">
+          <v-list-item v-for="order in orders" :key="order.id">
+            <template v-slot:prepend="{  }">
               <v-list-item-action>
-                <v-checkbox :input-value="isActive" color="primary" ></v-checkbox>
+                <v-checkbox
+                  :input-value="order.done"
+                  color="primary"
+                ></v-checkbox>
               </v-list-item-action>
             </template>
-            <v-list-item-title> Notifications </v-list-item-title>
-            <v-list-item-subtitle> Allow notifications </v-list-item-subtitle>
+            <v-list-item-title>{{ order.name }}</v-list-item-title>
+            <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
             <template v-slot:append>
               <v-list-item-action>
-                <v-btn class="primary"> Open </v-btn>
+                <v-btn class="primary" :to="'/ad/' + order.adId">Open</v-btn>
               </v-list-item-action>
             </template>
           </v-list-item>
@@ -23,15 +26,21 @@
     </v-row>
   </v-container>
 </template>
-  
+
 <script>
-  export default {
-    name: 'OrdersView',
-    date() {
-      return {
-  
-      }
-    }
+export default {
+  data() {
+    return {
+      orders: [
+        {
+          id: "123",
+          name: "Kostya",
+          phone: "+7(978)000-00-05",
+          adId: "1",
+          done: true,
+        },
+      ],
+    };
   }
+};
 </script>
-  
