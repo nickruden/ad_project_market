fb1793e0c3e63e90dffa41044b53d308098a3c05
nickruden <1nikitos.ru2018@gmail.com>
2023-03-15 17:55:28 +0300

    Вёрстка страницы списка объявлений пользователя

--- frontend/src/views/Ads/AdListView.vue
+++ frontend/src/views/Ads/AdListView.vue
@@ -1,19 +1,35 @@
 <template>
-    <v-container>
-      <v-row justify="center">
-        <h1>AdList</h1>
-      </v-row>
-    </v-container>
-  </template>
-  
-  <script>
-  export default {
-    name: 'AdListView',
-    date() {
+  <v-container>
+      <h1 class="text--secondary mb-3 mt-3">My ads</h1>
+      <v-card class="mx-auto" max-width="1000">
+          <v-row>
+              <v-img height="175px"></v-img>
+              <v-spacer></v-spacer>
+          </v-row>
+          <v-row>
+              <v-card-text>
+                  <h2 class="text--primary">{{  }}</h2>
+                  <p>{{  }}</p>
+              </v-card-text>
+              <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary" variant="text" >
+                      Open
+                  </v-btn>
+                  <v-spacer></v-spacer>
+              </v-card-actions>
+          </v-row>
+      </v-card>
+  </v-container>
+</template>
+
+
+<script>
+export default {
+  data() {
       return {
-  
+         
       }
-    }
   }
-  </script>
-  
+};
+</script>
