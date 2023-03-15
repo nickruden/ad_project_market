b341f39c5ec2223898d8f94887c59b1c46589800
nickruden <1nikitos.ru2018@gmail.com>
2023-03-15 17:59:33 +0300

    Подключение данных к списку

--- frontend/src/views/Ads/AdListView.vue
+++ frontend/src/views/Ads/AdListView.vue
@@ -1,23 +1,23 @@
 <template>
   <v-container>
-      <h1 class="text--secondary mb-3 mt-3">My ads</h1>
-      <v-card class="mx-auto" max-width="1000">
-          <v-row>
-              <v-img height="175px"></v-img>
+     <h1 class="text--secondary mb-3 mt-3">My ads</h1>
+     <v-card v-for="ad in ads" :key="ad.id" class="mx-auto" max-width="1000">
+        <v-row>
+           <v-img :src="ad.src" height="175px"></v-img>
+           <v-spacer></v-spacer>
+        </v-row>
+        <v-row>
+           <v-card-text>
+              <h2 class="text--primary">{{ ad.title }}</h2>
+              <p>{{ ad.desc }}</p>
+           </v-card-text>
+           <v-card-actions>
               <v-spacer></v-spacer>
-          </v-row>
-          <v-row>
-              <v-card-text>
-                  <h2 class="text--primary">{{  }}</h2>
-                  <p>{{  }}</p>
-              </v-card-text>
-              <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="primary" variant="text" >Open</v-btn>
-                  <v-spacer></v-spacer>
-              </v-card-actions>
-          </v-row>
-      </v-card>
+              <v-btn color="primary" variant="text" :to="'/ad/' + ad.id">Open</v-btn>
+              <v-spacer></v-spacer>
+           </v-card-actions>
+        </v-row>
+     </v-card>
   </v-container>
 </template>
 
@@ -25,9 +25,38 @@
 <script>
 export default {
   data() {
-      return {
-         
-      }
-  }
+     return {
+        ads: [
+           {
+              title: "First",
+              desc: "First Desc",
+              promo: true,
+              src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+              id: "1",
+           },
+           {
+              title: "Second",
+              desc: "Second Desc",
+              promo: true,
+              src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+              id: "2",
+           },
+           {
+              title: "Third",
+              desc: "Thitd Desc",
+              promo: true,
+              src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+              id: "3",
+           },
+           {
+              title: "Fouth",
+              desc: "Fouth Desc",
+              promo: true,
+              src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+              id: "4",
+           },
+        ],
+     };
+  },
 };
 </script>
