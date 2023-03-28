bd2d31316fd87944c973e4c6f68298e0eee3600f
nickruden <1nikitos.ru2018@gmail.com>
2023-03-28 21:25:26 +0300

    Подключение объявлений из store

--- frontend/src/views/Ads/AdListView.vue
+++ frontend/src/views/Ads/AdListView.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
      <h1 class="text--secondary mb-3 mt-3">My ads</h1>
-     <v-card v-for="ad in ads" :key="ad.id" class="mx-auto" max-width="1000">
+     <v-card v-for="ad in myAds" :key="ad.id" class="mx-auto" max-width="1000">
         <v-row>
            <v-img :src="ad.src" height="175px"></v-img>
            <v-spacer></v-spacer>
@@ -24,39 +24,10 @@
 
 <script>
 export default {
-  data() {
-     return {
-        ads: [
-           {
-              title: "First",
-              desc: "First Desc",
-              promo: true,
-              src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
-              id: "1",
-           },
-           {
-              title: "Second",
-              desc: "Second Desc",
-              promo: true,
-              src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-              id: "2",
-           },
-           {
-              title: "Third",
-              desc: "Thitd Desc",
-              promo: true,
-              src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
-              id: "3",
-           },
-           {
-              title: "Fouth",
-              desc: "Fouth Desc",
-              promo: true,
-              src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
-              id: "4",
-           },
-        ],
-     };
-  },
+  computed: {
+   myAds() {
+      return this.$store.getters.myAds
+   }
+  }
 };
 </script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <v-row justify="center">
       <v-col cols="12" xs="12">
         <v-carousel>
-          <v-carousel-item v-for="ad in ads" :key="ad.id" :src="ad.src" cover>
+          <v-carousel-item v-for="ad in promoAds" :key="ad.id" :src="ad.src" cover>
             <div class="ad-link">
               <v-btn class="error" :to="'/ad/' + ad.id">
                 {{ ad.title }}
@@ -39,38 +39,12 @@
 <script>
 export default {
   name: 'HomeView',
-  data() {
-    return {
-      ads: [
-        {
-          title: "First",
-          desc: "First Desc",
-          promo: true,
-          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
-          id: "1"
-        },
-        {
-          title: "Second",
-          desc: "Second Desc",
-          promo: true,
-          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-          id: "2"
-        },
-        {
-          title: "Third",
-          desc: "Thitd Desc",
-          promo: true,
-          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
-          id: "3"
-        },
-        {
-          title: "Fouth",
-          desc: "Fouth Desc",
-          promo: true,
-          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
-          id: "4"
-        }
-      ]
+  computed: {
+    promoAds() {
+      return this.$store.getters.promoAds
+    },
+    ads() {
+      return this.$store.getters.ads
     }
   }
 }
