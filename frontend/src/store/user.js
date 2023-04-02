c21c2b973faf492fe2fecf3a739d959f889aecfc
nickruden <1nikitos.ru2018@gmail.com>
2023-04-02 17:56:39 +0300

    Создание action и mutation  для регистрации пользователя

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -1,9 +1,27 @@
+class User {
+    constructor(id, email, password) {
+        this.id = id
+        this.email = email
+        this.password = password
+    }
+}
+    
 export default {
 	state: {
         user: null
     },
-	mutations: {},
-	actions: {},
+	mutations: {
+        setUser(state, payload) {
+            console.log(payload)
+            state.user = payload
+        }
+    },
+	actions: {
+        registerUser({commit}, {email, password}) {
+            //Здесь запрос на сервер для регистрации
+            commit('setUser', new User(1, email, password))
+        }
+    },
 	getters: {
         user(state) {
             return state.user;
