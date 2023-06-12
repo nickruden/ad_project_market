037db740ec81c33dcc1e8f48f82722953574b1a2
nickruden <1nikitos.ru2018@gmail.com>
2023-06-12 13:18:06 +0300

    Верстка снекбара и логика отображения ошибок

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -39,6 +39,19 @@
     <v-main>
       <router-view></router-view>
     </v-main>
+    <v-snackbar 
+      v-model="error"
+      multi-line
+      :timeout="2000"
+      color="primary"
+    > {{ error }}
+    <template v-slot:actions>
+      <v-btn
+        variant="text"
+        @click="closeError"
+      > Close </v-btn>
+    </template>
+  </v-snackbar>
   </v-app>
 </template>
 
@@ -54,6 +67,18 @@
           {title:"New ad", icon:"mdi-note-plus-outline", url:"/new"},
           {title:"My ads", icon:"mdi-view-list-outline", url:"/list"}
         ]
+      }
+    },
+
+    computed: {
+      error () {
+        return this.$store.getters.error
+      }
+    },
+
+    methods: {
+      closeError () {
+        this.$store.dispatch('clearError')
       }
     }
   }
