5e1bc1495064839b159791f5c0cda4660cb6cb6d
nickruden <1nikitos.ru2018@gmail.com>
2023-06-12 12:33:25 +0300

    Рефакторинг action «registerUser»

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -18,8 +18,16 @@ export default {
     },
 	actions: {
         registerUser({commit}, {email, password}) {
-            //Здесь запрос на сервер для регистрации
-            commit('setUser', new User(1, email, password))
+            commit('clearError')
+            commit('setLoading', true)
+            fb.auth().createUserWithEmailAndPassword(email,password).then(response => {
+                commit('setUser', new User(response.user.uid))
+                commit('setLoading', false)
+            }).catch( error => {
+                commit('setLoading', false)
+                commit('setError', error.message)
+                throw error
+            })
         }
     },
 	getters: {
