faa2801a8813bb413c3316069054d56ac2f680a8
nickruden <1nikitos.ru2018@gmail.com>
2023-06-12 13:12:23 +0300

    Подключение лоудера на экране логина

--- frontend/src/views/Auth/LoginView.vue
+++ frontend/src/views/Auth/LoginView.vue
@@ -14,7 +14,7 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" @click="onSubmit" :disabled="!valid">Login</v-btn>
+            <v-btn color="primary" @click="onSubmit" :loading="loading" :disabled="!valid||loading">Login</v-btn>
           </v-card-actions>	
         </v-card>
       </v-col>
@@ -40,6 +40,12 @@ export default {
 			]
 		} 	
 	},
+
+  computed: {
+    loading() {
+      return this.$store.getters.loading
+    }
+  },
 
   methods: {
     onSubmit(){
