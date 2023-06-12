c53277e16412f185136975000a46fec91a78ba2f
nickruden <1nikitos.ru2018@gmail.com>
2023-06-12 12:56:37 +0300

    Создание «loginUser» action

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -36,7 +36,29 @@ export default {
                     throw 'Упс... Ошибка регистрации'
                 })
             }
-        }
+        },
+
+        async loginUser({commit},{email, password}) {
+            commit('clearError')
+            commit('setLoading', true)
+            //Здесь выполняется запрос на сервер
+            let isRequestOk = false
+            let promise = new Promise(function(resolve) {setTimeout(() => resolve('Done'), 3000);});
+            
+            
+            if (isRequestOk) {
+                await promise.then(()=> {
+                    commit('setUser', new User(1, email, password))
+                    commit('setLoading', false)
+                })
+            } else {
+                await promise.then(()=> {
+                    commit('setLoading', false)
+                    commit('setError', 'Ошибка логина или пароля')
+                    throw 'Упс... Ошибка логина или пароля'
+                })
+            }
+        }     
     },
 	getters: {
         user(state) {
