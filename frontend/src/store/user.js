3fe6131bb63e75027473a215cb4e9a46ac96c26a
nickruden <1nikitos.ru2018@gmail.com>
2023-06-13 11:07:01 +0300

    Создание getter isUserLoggedIn

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -63,6 +63,10 @@ export default {
 	getters: {
         user(state) {
             return state.user;
-        }
+        },
+        
+        isUserLoggedIn (state) {
+            return state.user !== null
+        }        
     }
 }
