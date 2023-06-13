e0ef33f1358f174ab006f843b5d8c89e0ebb803c
nickruden <1nikitos.ru2018@gmail.com>
2023-06-13 20:53:44 +0300

    Верстка диалогового окна

---
+++ frontend/src/views/Ads/EditAdModal.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-dialog v-model="modal" width="400px">
+        <template v-slot:activator="{ props }">
+            <v-btn v-bind="props" color="warning">Edit</v-btn>
+        </template>
+        <v-card class="pa-3">
+            <v-row justify="center">
+                <v-col cols="12">
+                    <v-card-title>
+                        <h1 class="text--primary">Edit Ad</h1>
+                    </v-card-title>
+                </v-col>
+            </v-row>
+            <v-row justify="center">
+                <v-col cols="12">
+                    <v-card-text>Lorem ipsum.</v-card-text>
+                </v-col>
+            </v-row>
+
+            <v-row justify="center">
+                <v-col cols="12">
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn>Cancel</v-btn>
+                        <v-btn color="success">Save</v-btn>
+                    </v-card-actions>
+                </v-col>
+            </v-row>
+        </v-card>
+    </v-dialog>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            modal: false
+        }
+    }
+}
+</script>
