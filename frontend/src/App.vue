7b1de791a051225e69af721d1c05de5ff484e4c1
nickruden <1nikitos.ru2018@gmail.com>
2023-06-13 11:30:09 +0300

    Logout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,8 +21,16 @@
           </template>
           <v-list-item-title>{{ link.title }}</v-list-item-title>
         </v-list-item>
+        <v-list-item @click="onLogout" v-if="isUserLoggedIn">
+          <template v-slot:prepend>
+            <v-icon icon="mdi-exit-to-app"></v-icon>
+          </template>
+          <v-list-item-title>Logout</v-list-item-title>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer> 
+
+
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>
@@ -31,27 +39,25 @@
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn v-for="link in links" :key="link.title" :to="link.url">
-          <v-icon start :icon="link.icon"></v-icon> 
-          {{ link.title }} 
-        </v-btn> 
+          <v-icon start :icon="link.icon"></v-icon>
+          {{ link.title }}
+        </v-btn>
+        <v-btn @click="onLogout" v-if="isUserLoggedIn"> <v-icon start icon="mdi-exit-to-app"></v-icon> Logout </v-btn>
       </v-toolbar-items>
     </v-app-bar>
+
+
     <v-main>
       <router-view></router-view>
     </v-main>
-    <v-snackbar 
-      v-model="error"
-      multi-line
-      :timeout="2000"
-      color="primary"
-    > {{ error }}
-    <template v-slot:actions>
-      <v-btn
-        variant="text"
-        @click="closeError"
-      > Close </v-btn>
-    </template>
-  </v-snackbar>
+
+
+    <v-snackbar v-model="error" multi-line :timeout="2000" color="primary"> 
+      {{ error }}
+      <template v-slot:actions>
+      <v-btn variant="text" @click="closeError"> Close </v-btn>
+      </template>
+    </v-snackbar>
   </v-app>
 </template>
 
@@ -89,6 +95,11 @@
     methods: {
       closeError () {
         this.$store.dispatch('clearError')
+      },
+      
+      onLogout() {
+        this.$store.dispatch("logoutUser")
+        this.$router.push("/")
       }
     }
   }
--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -58,7 +58,11 @@ export default {
                     throw 'Упс... Ошибка логина или пароля'
                 })
             }
-        }     
+        },
+
+        logoutUser({commit}) {
+            commit('setUser', null)
+        }
     },
 	getters: {
         user(state) {
