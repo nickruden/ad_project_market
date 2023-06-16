aad41f4306de93631cc4d7a1ab0fab8777ba49e9
nickruden <1nikitos.ru2018@gmail.com>
2023-06-16 13:53:08 +0300

    Локальный лоудер для компонента BuyAdModel

--- frontend/src/views/Shared/BuyAdModal.vue
+++ frontend/src/views/Shared/BuyAdModal.vue
@@ -25,8 +25,8 @@
                 <v-col cols="12">
                     <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn @click="onClose">Close</v-btn>
-                        <v-btn @click="onSave" color="success">Buy It!</v-btn>
+                        <v-btn @click="onClose" :disabled="localLoading" >Close</v-btn>
+                        <v-btn @click="onSave" color="success" :disabled="localLoading" :loading="localLoading">Buy It!</v-btn>
                     </v-card-actions>
                 </v-col>
             </v-row>
@@ -41,7 +41,8 @@ export default {
         return {
             modal: false,
             name: "",
-            phone: ""
+            phone: "",
+            localLoading: false
         }
     },
     methods: {
@@ -52,6 +53,7 @@ export default {
         },
         onSave (){
             if (this.name !== '' && this.phone !== '') {
+                this.localLoading = true
                 this.$store.dispatch('createOrder', {
                         name: this.name,
 						phone: this.phone,
@@ -59,6 +61,7 @@ export default {
 						userId: this.ad.userId
 					})
 					.finally(() => {
+                        this.localLoading = false
 						this.name = ""
 						this.phone = ""
 						this.modal = false
