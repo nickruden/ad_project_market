e2a9de214cb143304d883d6fae7e7d339893676b
nickruden <1nikitos.ru2018@gmail.com>
2023-06-16 13:58:16 +0300

    Создание store заказов

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,10 +2,11 @@ import { createStore } from 'vuex'
 import ads from './ads'
 import user from './user'
 import shared from './shared'
+import orders from './orders'
 
 const store = createStore({
     modules: {
-        ads, user, shared
+        ads, user, shared, orders
     }
 })
 
