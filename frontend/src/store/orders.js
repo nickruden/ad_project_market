64fd6ca5b9ffacaee98d70b175e0d3b5d27bebfc
nickruden <1nikitos.ru2018@gmail.com>
2023-06-16 14:01:16 +0300

    Создание заказа

--- frontend/src/store/orders.js
+++ frontend/src/store/orders.js
@@ -1,21 +1,34 @@
+class Order {
+    constructor(name, phone, adId, userId, done = false, id = null) {
+        this.name = name
+        this.phone = phone
+        this.adId = adId
+        this.userId = userId
+        this.done = done
+        this.id = id
+    }
+}    
+
+
 export default {
 	state: {
         orders: []
     },
-	mutations: {},
+	mutations: {
+        createOrder(state, payload){
+            state.orders.push(payload)
+        }            
+    },
 	actions: {
         async createOrder({commit},{name, phone, adId, userId}) {
-            console.log(name)
-            console.log(phone)
-            console.log(adId)
-            console.log(userId)
-            commit('clearError')
+            let payload = new Order(name, phone, adId, userId, false, Math.random())
             //Заглушка запроса
             let isRequestOk = true
             let promise = new Promise(function(resolve) {setTimeout(() => resolve('Done'), 3000);});
             if (isRequestOk) {
                 await promise.then(()=> {
                     //Здесь вызовем commit для добавления заказа
+                    commit('createOrder', payload)
                 })
             } else {
                 await promise.then(()=> {
